e95ed56c1af289298f6c318e1f762ae098e7a1b8
probably-not-porter <pelibby16@earlham.edu>
2020-11-11 16:19:33 -0500

    init

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+
+app.set('view engine', 'ejs');
+app.get('/', function(req, res) {
+    res.render("index",{
+    })
+});
+app.use(express.static(__dirname + '/public'));
+
+
+// LISTEN
+app.listen(process.env.PORT || 9900, function(){
+    console.log("Server: Running on port %d in %s mode", this.address().port, app.settings.env);
+});
